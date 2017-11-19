8f9444dcf9a73926d699da868d8dd2cc2b400020
Christoph Chalfin <scheuermann.chr@gmail.com>
2017-11-19 20:36:46 +0100

    Added Buttons to show and hide certain types of transportation and changed the layout of the line, being shown.

--- timeTable.py
+++ timeTable.py
@@ -7,7 +7,7 @@ import time
 idStation = mvg_api.get_id_for_station("Ostbahnhof")
 
 #Set Types to show
-typesToShow=['u', 'b']
+typesToShow=['u', 'b', 's']
 
 #Start TKinter dialog
 top = Tk()
@@ -19,15 +19,39 @@ class myThread (threading.Thread):
    def run(self):
       while not stopEvent.is_set():
          updateTimes()
-         stopEvent.wait(10)
+         stopEvent.wait(1)
      
    
 def exitButton():
+   print "Exit"
    stopEvent.set()
    top.destroy()
 
+def busButton():
+   print "BusButton"
+    
+   if 'b' in typesToShow:
+      typesToShow.remove('b')
+   else:
+      typesToShow.append('b')
+      
+   print typesToShow
+
 def subwayButton():
    print "Test"
+   
+   if 'u' in typesToShow:
+      typesToShow.remove('u')
+   else:
+      typesToShow.append('u')
+      
+def sbahnButton():
+   print "Test"
+   
+   if 's' in typesToShow:
+      typesToShow.remove('s')
+   else:
+      typesToShow.append('s')
 
 def updateTimes():
 
@@ -38,6 +62,8 @@ def updateTimes():
 
    depNumber=0
    for departure in departures:
+      
+      #Get information from mvg_api
       strDestination = departure['destination']
       strProduct = departure['product']
       strLabel = departure['label']
@@ -47,10 +73,19 @@ def updateTimes():
       strText = strProduct, strLabel, strDestination, strDepartureMin
       
       if strProduct in typesToShow:
-         var = labelContent[depNumber]
-         label = labels[depNumber]
-         var.set(strText)
-         label.config(bg=strLineBackgroundColor)
+         
+         timeEntryDict = timeTableEntries[depNumber]
+         varLine = timeEntryDict['Line']
+         varLine.set(strProduct + strLabel)
+         labelLine = timeEntryDict['labelLine']
+         labelLine.config(bg=strLineBackgroundColor)
+         
+         varDestination = timeEntryDict['Destination']
+         varDestination.set(strDestination)
+         
+         varTimeLeft = timeEntryDict['TimeLeft']
+         varTimeLeft.set(strDepartureMin)          
+         
          depNumber += 1
 
 #Make it resizeable, when window is expanded
@@ -63,11 +98,11 @@ typeFrame.grid_columnconfigure(0, weight=1)
 typeFrame.grid_rowconfigure(0, weight=1)
 typeFrame.grid(row=0, column=0, sticky="nsew")
 
-subwayButton = Button(typeFrame, text="U-Bahn")
-subwayButton.pack(fill = X, expand=1)
+#subwayButton = Button(typeFrame, text="U-Bahn")
+#subwayButton.pack(fill = X, expand=1)
 
-busButton = Button(typeFrame, text="Bus")
-busButton.pack(fill = X, expand=1)
+#busButton = Button(typeFrame, text="Bus")
+#busButton.pack(fill = X, expand=1)
 
 #Frame
 listFrame = Frame(top)
@@ -77,27 +112,49 @@ listFrame.grid(row=0, column=0, sticky="nsew")
 
 #Define Buttons
 b = Button(listFrame, text="Exit", command=exitButton)
-b.pack( fill=X, expand=0 )
+b.grid(row=0, column=0, columnspan=3, sticky='we')
+
+buttonBus = Button(listFrame, text="Bus", command=busButton)
+buttonBus.grid(row=1, column=0, sticky='we')
+
+buttonSubway = Button(listFrame, text="U-Bahn", command=subwayButton)
+buttonSubway.grid(row=1, column=1, sticky='we')
+
+buttonSBahn = Button(listFrame, text="S-Bahn", command=sbahnButton)
+buttonSBahn.grid(row=1, column=2, sticky='we')
 
 #Define Frame to hold Labels for lines to whow
 labelFrame = Frame(listFrame)
-labelFrame.pack(fill=BOTH, expand=1)
+labelFrame.grid(row=2, column=0, columnspan=3, sticky='we')
 
+timeTableEntries = []
 labelContent = []
 labels = []
 for i in range(30):
-   var = StringVar()
-   var.set(i)
-   labelContent.append(var)
-   label=Label(labelFrame, textvariable=var)
-   label.grid_columnconfigure(0, weight=1, pad=1000)
-   label.grid(row=i, column=0, sticky='w')
-   labels.append(label)
+   varLine = StringVar()
+   varLine.set('L')
+   labelLine = Label(labelFrame, textvariable=varLine, fg='white', font=("Arial", 12))
+   labelLine.grid(row=i, column=0, sticky='we')
+   
+   varDestination = StringVar()
+   varDestination.set('D')
+   labelDestination = Label(labelFrame, textvariable=varDestination, font=("Arial", 12))
+   labelDestination.grid(row=i, column=1, sticky='w')
+   
+   varTimeLeft = StringVar()
+   varTimeLeft.set('T')
+   labelTimeLeft = Label(labelFrame, textvariable=varTimeLeft, font=("Arial", 12))
+   labelTimeLeft.grid(row=i, column=2, sticky='we')
+   
+   timeEntryDict = {'Line' : varLine, 'labelLine' : labelLine, 'Destination' : varDestination, 'labelDestination' : labelDestination,
+                    'TimeLeft' : varTimeLeft, 'labelTimeLeft' : labelTimeLeft}
+   
+   timeTableEntries.append(timeEntryDict)
 
 #Start UpdateThread
 stopEvent = threading.Event()
 thread = myThread()
 thread.start()
 
-top.attributes('-fullscreen', True)
+#top.attributes('-fullscreen', True)
 top.mainloop()
